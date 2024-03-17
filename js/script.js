12fa71e689f8510f1082c09d9fbf2c0fb6c658e3
Mahelstrom77 <newmew10@yahoo.com>
2024-03-17 09:21:19 -0400

    took out old data and entered in new data

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood='Pasta';
-document.write(favoriteFood);
+let pokemonList=[
+    {name: "pikachu", height: 0.4, type: ['electric']},
+    {name: "vileplume", height: 1.2, type: ['grass', 'poison']},
+    {name: "gengar", height: 1.5, type: ['ghost', 'poison']}
+]
