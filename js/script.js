cc18a11cd79ee36a5f55b00482b3b1d5f6e4c1b2
Mahelstrom77 <newmew10@yahoo.com>
2024-03-19 16:29:43 -0400

    fixed coding

--- js/script.js
+++ js/script.js
@@ -4,62 +4,13 @@ let pokemonList=[
     {name: "gengar", height: 1.5, type: ['ghost', 'poison']}
 ]
 
-if (height===0.4){
-    console.log('You are pikachu!');
-} else if (type===electric){
-    console.log('You are pikachu!');
-} else{
-    console.log('You are not pikachu!');
-
-    document.write(Pikachu);
-
-    let name='pikachu';
-    let height="0.4";
-    console.log(name+height)
-}
-
-if(height===1.2){
-    console.log('You are vileplume!');
-} else if(type===grass,poison){
-    console.log('You are vileplume!');
-} else{
-    console.log('You are not vileplume!');
-
-    document.write(Vileplume);
-
-    let name='vileplume';
-    let height="1.2";
-    console.log(name+height)
-}
-
-if(height===1.5){
-    console.log('You are gengar!');
-} else if(type===ghost,poison){
-    console.log('You are gengar!');
-} else{
-    console.log('You are not gengar!');
-
-    document.write(Gengar);
-
-    let name='gengar';
-    let height="1.5";
-    console.log(name+height)
-}
-
-let repository= [
-    {
-        name: "Pikachu",
-        height: 0.4,
-        type:["electric"]
-    },
-    {
-        name:"Vileplume",
-        height:"1.2",
-        types:["grass","poison"]    
-    },
-    {
-        name:"Gengar",
-        height:"1.5",
-        types:["ghost","poison"]
+let pokemonRepo = ('function')
+function add(pokemon){
+    if(
+        typeof pokemon==='height' && 'name' in pokemon
+    ){
+        pokemonList.push(pokemon);
+    } else{
+        console.error('pokemon is not correct');
     }
-];
+}
