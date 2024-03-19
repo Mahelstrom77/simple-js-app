5162707d0acd7104b8a220bfad47b3ee1ef039cb
Mahelstrom77 <newmew10@yahoo.com>
2024-03-19 15:40:49 -0400

    added in conditionals

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@ let pokemonList=[
     {name: "vileplume", height: 1.2, type: ['grass', 'poison']},
     {name: "gengar", height: 1.5, type: ['ghost', 'poison']}
 ]
+
+if (height===0.4){
+    console.log('You are pikachu!');
+} else if (type===electric){
+    console.log('You are pikachu!');
+} else{
+    console.log('You are not pikachu!');
+}
+
+if(height===1.2){
+    console.log('You are vileplume!');
+} else if(type===grass,poison){
+    console.log('You are vileplume!');
+} else{
+    console.log('You are not vileplume!');
+}
+
+if(height===1.5){
+    console.log('You are gengar!');
+} else if(type===ghost,poison){
+    console.log('You are gengar!');
+} else{
+    console.log('You are not gengar!')
+}
