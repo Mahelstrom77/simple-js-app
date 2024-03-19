4e08aa01a490b83e5764a1a7333a6b58ada7c150
Mahelstrom77 <newmew10@yahoo.com>
2024-03-19 17:23:43 -0400

    added string

--- js/script.js
+++ js/script.js
@@ -8,9 +8,21 @@ let pokemonList=[
 for(let i=0; i<pokemonList.length; i++){
     if(pokemonList[i].height<1){
     console.log(pokemonList[i].name + " is Pikachu");
+    document.write(Pikachu)
+    let name='Pikachu';
+    let message= (height=0.4);
+    console.log(name+message);
     }else if (pokemonList[i].height>1 && pokemonList[i].height<1.4);{
     console.log(pokemonList[i].name + " is Vileplume");
+    document.write(Vileplume)
+    let name='Vileplume';
+    let message= (height=1.4);
+    console.log(name+message);
     }else {
     console.log(pokemonList[i].name + " is Gengar");
+    document.write(Gengar)
+    let name='Gengar';
+    let message= (height=1.5);
+    console.log(name+message);
     }
 }
