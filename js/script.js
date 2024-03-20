ddf796d1210aae375102369ece10e6494df5c423
Mahelstrom77 <newmew10@yahoo.com>
2024-03-20 00:29:45 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ for(let i=0; i<pokemonList.length; i++){
     if (pokemonList[i].height>0.1 && pokemonList[i].height<=0.4){
     console.log(pokemonList[i].name + " is Pikachu");
 
-    }if (pokemonList[i].height>1 && pokemonList[i].height<1.4);{
+    }if (pokemonList[i].height>1 || pokemonList[i].height<1.4);{
     console.log(pokemonList[i].name + " is Vileplume");
     }
 
