f3c833736985ee5d69ed4791e97a0457d0acaf4d
Mahelstrom77 <newmew10@yahoo.com>
2024-03-20 00:36:44 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ console.log(pokemonList[i])
     console.log(pokemonList[i].name + " is Vileplume");
     }
 
-    if (pokemonList[i].height>1.4 && pokemonList[i].height<2){
+    if (pokemonList[i].height>=1.5 && pokemonList[i].height<2){
     console.log(pokemonList[i].name + " is Gengar");
         }
     }
