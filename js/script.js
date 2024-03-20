407d28194204b9180786be9fcfc53306157d0500
Mahelstrom77 <newmew10@yahoo.com>
2024-03-20 07:47:33 -0400

    cleaned up code

--- js/script.js
+++ js/script.js
@@ -15,23 +15,25 @@ let name3='Gengar';
 
 
 for(let i=0; i<pokemonList.length; i++){
-console.log(pokemonList[i])
-    if (pokemonList[i].height >0 && pokemonList[i].height<=1){
+    console.log(pokemonList[i])
+
+if (pokemonList[i].height >0 && pokemonList[i].height<=1){
             console.log(pokemonList[i].name + " is Pikachu");
                 document.write(name1,name4)
                     
 }
 
-     if (pokemonList[i].height>1 && pokemonList[i].height<2.4){
+if (pokemonList[i].height>1 && pokemonList[i].height<2.4){
             console.log(pokemonList[i].name + " is Vileplume");
                 document.write(name2,name5)
     }
 
-    if (pokemonList[i].height>2 && pokemonList[i].height<3){
+if (pokemonList[i].height>2 && pokemonList[i].height<3){
             console.log(pokemonList[i].name + " is Gengar");
                 document.write(name3,name6)
         }
-    if(pokemonList[i].height>2.1){
+
+if(pokemonList[i].height>2.1){
         console.log('Wow, your big!')
     }
 }
