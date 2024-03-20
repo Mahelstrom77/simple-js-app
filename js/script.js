9ab0f918903eb57e0b4fc54f6635348e1eb1c3d1
Mahelstrom77 <newmew10@yahoo.com>
2024-03-20 07:28:35 -0400

    added document names to dom

--- js/script.js
+++ js/script.js
@@ -3,23 +3,25 @@ let pokemonList=[
     {name: "vileplume", height: 2, type: ['grass', 'poison']},
     {name: "gengar", height: 2.5, type: ['ghost', 'poison']}
 ]
-let name1='pikachu';
-let name2='vileplume';
-letname3='gengar';
+let name1='Pikachu';
+let name2='Vileplume';
+letname3='Gengar';
 
 for(let i=0; i<pokemonList.length; i++){
 console.log(pokemonList[i])
     if (pokemonList[i].height >0 && pokemonList[i].height<=1){
-    console.log(pokemonList[i].name + " is Pikachu");
-    document.write(name1)
+            console.log(pokemonList[i].name + " is Pikachu");
+                document.write(name1)
 }
 
      if (pokemonList[i].height>1 && pokemonList[i].height<2.4){
-        console.log(pokemonList[i].name + " is Vileplume");
+            console.log(pokemonList[i].name + " is Vileplume");
+                document.write(name2)
     }
 
     if (pokemonList[i].height>2 && pokemonList[i].height<3){
-    console.log(pokemonList[i].name + " is Gengar");
+            console.log(pokemonList[i].name + " is Gengar");
+                document.write(name3)
         }
 }
    
