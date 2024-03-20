dfe51f13a045142e4b2b4d0e47b26ac95b95555d
Mahelstrom77 <newmew10@yahoo.com>
2024-03-20 07:33:26 -0400

    added height next to their names

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ let pokemonList=[
 let name1='Pikachu';
 let name4="(height:7)"
 let name2='Vileplume';
+let name5="(height:2)"
 let name3='Gengar';
+let name6="(height:2.5)"
 
 for(let i=0; i<pokemonList.length; i++){
 console.log(pokemonList[i])
@@ -18,12 +20,12 @@ console.log(pokemonList[i])
 
      if (pokemonList[i].height>1 && pokemonList[i].height<2.4){
             console.log(pokemonList[i].name + " is Vileplume");
-                document.write(name2)
+                document.write(name2,name5)
     }
 
     if (pokemonList[i].height>2 && pokemonList[i].height<3){
             console.log(pokemonList[i].name + " is Gengar");
-                document.write(name3)
+                document.write(name3,name6)
         }
 }
    
