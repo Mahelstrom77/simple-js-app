be06205527b3f0feb998e7b228d1a19d7fb79bfa
Mahelstrom77 <newmew10@yahoo.com>
2024-03-20 07:33:47 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@ let pokemonList=[
     {name: "gengar", height: 2.5, type: ['ghost', 'poison']}
 ]
 let name1='Pikachu';
-let name4="(height:7)"
+    let name4="(height:7)"
+
 let name2='Vileplume';
-let name5="(height:2)"
+    let name5="(height:2)"
+
 let name3='Gengar';
-let name6="(height:2.5)"
+    let name6="(height:2.5)"
 
 for(let i=0; i<pokemonList.length; i++){
 console.log(pokemonList[i])
