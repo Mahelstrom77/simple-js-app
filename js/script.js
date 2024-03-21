102112d11cf165f3559fab2e02f4e03805ff23ca
Mahelstrom77 <newmew10@yahoo.com>
2024-03-21 09:38:06 -0400

    changed code

--- js/script.js
+++ js/script.js
@@ -5,26 +5,16 @@ let pokemonList=[
     {name: "gengar", height: 2.5, type: ['ghost', 'poison']}
 ]
 //start of my loop//
-for(let i=0; i<pokemonList.length; i++){
+for(let i=0; i<pokemonList.height; i++){
     console.log(pokemonList[i])
-
-if (pokemonList[i].height >0 && pokemonList[i].height<=1){
-            console.log(pokemonList[i].name + " is Pikachu");
-                document.write(pokemonList[i].name + "(height:1)")                 
+    
+if(pokemonList[i].height<2){
+    console.log(pokemonList.name +"is Pikachu");
+}  
+    else if (pokemonList[i].height>1 && pokemonList.height<2.5){
+        console.log(pokemonList[i].name + "is Vileplume");
     }
-
-if (pokemonList[i].height>1 && pokemonList[i].height<2.4){
-            console.log(pokemonList[i].name + " is Vileplume");
-                document.write(pokemonList[i].name + "(height:2)")
-    }
-
-if (pokemonList[i].height>2 && pokemonList[i].height<3){
-            console.log(pokemonList[i].name + " is Gengar");
-                document.write(pokemonList[i].name + "(height:2.5)");
-    }
-    //conditional to tell that this pokemon is big//
-if(pokemonList[i].height>2.1){
-        document.write('Wow, your big!')
-    }
+        else{
+            console.log(pokemonList[i].name +"is Gengar");
+        }
 }
-   
