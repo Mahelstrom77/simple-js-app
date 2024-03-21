05d5ec58cc62907421fa6ea750af2892c14db754
Mahelstrom77 <newmew10@yahoo.com>
2024-03-21 10:13:08 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,25 +4,17 @@ let pokemonList=[
     {name: "vileplume", height: 2, type: ['grass', 'poison']},
     {name: "gengar", height: 2.5, type: ['ghost', 'poison']}
 ];
-//this is for my strings//
-let text1='(height:1)'
-let text2='(height:2)'
-let text3='(height:2.5)'
-
-
 //start of my loop//
 for(let i=0; i<pokemonList.length; i++)
 {
     if(pokemonList[i].height<=1){
         console.log(pokemonList[i].name +" is pikachu");
-        document.write(pokemonList[i].name)+(text1[i].height)
+        document.write(pokemonList[i].name)
     }
     else if(pokemonList[i].height>1 && pokemonList[i].height<2.5){
-        console.log(pokemonList[i].name + " is vileplume");
-        document.write(pokemonList[i].name+ "(height:2)")
+        console.log(pokemonList[i].name + " is vileplume");  
     }
     else{
         console.log(pokemonList[i].name + " is gengar");
-        document.write(pokemonList[i].name+ "(height:2.5)")
     }
 }
