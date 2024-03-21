3fa33343255a3ea7f96e248a066c3e2da2dde4dd
Mahelstrom77 <newmew10@yahoo.com>
2024-03-21 10:03:02 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,14 +8,14 @@ let pokemonList=[
 for(let i=0; i<pokemonList.length; i++)
 {
     if(pokemonList[i].height<=1){
-        document.write(pokemonList[i].name +" is pikachu");
+        document.write(pokemonList[i].name +"pikachu");
     }
 
     else if(pokemonList[i].height>1 && pokemonList[i].height<2.5){
-        document.write(pokemonList[i].name + " is vileplume");  
+        document.write(pokemonList[i].name + "vileplume");  
     }
     
     else{
-        document.write(pokemonList[i].name + " is gengar");
+        document.write(pokemonList[i].name + "gengar");
     }
 }
